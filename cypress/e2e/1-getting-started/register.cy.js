c4592652d01abbeea2dbf1cbd8d0c3449b7a48fc
Isabella Salles <bellssalles@gmail.com>
2023-12-12 14:36:45 -0300

    :tada: Initial commit

---
+++ cypress/e2e/1-getting-started/register.cy.js
@@ -0,0 +1,5 @@
+describe('register page', () => {
+    it('should show validation errors when leaving all fields blank', () => {
+        cy.visit('https://paciente.lacreisaude.com.br/')
+    })
+})

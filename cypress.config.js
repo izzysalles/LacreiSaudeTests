6805f8be52bb9e071d0138f6d86a66ff5768d383
Isabella Salles <bellssalles@gmail.com>
2023-12-13 16:17:45 -0300

    :bug: fix mocha awesome html generation

--- cypress.config.js
+++ cypress.config.js
@@ -1,26 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      module.exports = defineConfig({
-        e2e: {
-          setupNodeEvents(on, config) {
-            {
-              ("reporter");
-              "mochawesome", "reporterOptions";
-              {
-                ("reportDir");
-                "cypress/report/mochawesome-report", "overwrite";
-                true, "html";
-                true, "json";
-                false, "timestamp";
-                ("mmddyyyy_HHMMss");
-              }
-            }
-          },
-        },
-      });
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,6 +1,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
+import 'cypress-mochawesome-reporter/register'
 
 Cypress.on('uncaught:exception', (err, runnable) => {
-    return false
-  })
+  return false;
+});
